96eb0b38a4649de17c5ea44e3b79d7bfc5a448a5
ValentynaVel <110202655+ValentynaVel@users.noreply.github.com>
2022-12-10 20:34:53 +0000

    adding homework 4 file

---
+++ homework4.py
@@ -0,0 +1,32 @@
+# Task description:
+# 1. Створити програму, яка буде очікувати введення тексту від користувача і повідомляти — чи є введений текст “числом” чи “словом”.
+# 2. Якщо введений текст “число”, програма має також вказати чи є воно парним чи непарним.
+# 3. Якщо це “слово”, програма має вказати його довжину.
+
+user_input = input("Enter number or word: ")
+
+if user_input.isdigit():
+    number = int(user_input)
+
+    def is_even():
+        return number % 2 == 0
+    """This function checks if the number is even or odd."""
+
+    if is_even():
+        print("This is number " + str(number) + ". It is even.")
+    else:
+        print("This is number " + str(number) + ". It is odd.")
+else:
+    print("This is word. It contains " + str(len(user_input)) + " letters.")
+
+
+
+
+
+
+
+
+
+
+
+

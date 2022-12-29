ec682dfe093e10bae0eaee67d70fd8faf3170add
ValentynaVel <110202655+ValentynaVel@users.noreply.github.com>
2022-12-29 23:56:38 +0000

    Updating homework #6

--- homework6.py
+++ homework6.py
@@ -12,36 +12,46 @@ my_contacts = {
     'Winston Churchill': '+445566778899'
 }
 
-command = input("Enter command (stats, add, delete, list, show): ")
-# format "command amount"
+while True:
+    command = input("Enter command (stats, add, delete, list, show): ")
+    # format "command amount"
 
-if command == 'stats':
-    print(len(my_contacts))
+    if command == 'stats':
+        print(len(my_contacts))
     # calculating the number of contacts
 
-elif command == 'add':
-    new_contact_name = input('Type name and surname: ')
-    new_contact_phone = input('Type phone number: ')
-    my_contacts[new_contact_name] = new_contact_phone
-    print(my_contacts)
+    elif command == 'add':
+        new_contact_name = input('Type name and surname: ')
+        if new_contact_name in my_contacts:
+            print("This contact is already in the contact list.")
+        else:
+            new_contact_phone = input('Type phone number: ')
+            my_contacts[new_contact_name] = new_contact_phone
+            print(my_contacts)
     # adding new contact
 
-elif command == 'delete':
-    contact_to_delete = input('Enter name and surname: ')
-    del my_contacts[contact_to_delete]
-    print(my_contacts)
+    elif command == 'delete':
+        contact_to_delete = input('Enter name and surname: ')
+        if contact_to_delete in my_contacts:
+            del my_contacts[contact_to_delete]
+            print(my_contacts)
+        else:
+            print("This contact does not exist. Nothing to delete.")
     # deleting contact by name
 
-elif command == 'list':
-    for key, value in my_contacts.items():
-        print(key, value)
+    elif command == 'list':
+        for key, value in my_contacts.items():
+            print(key, value)
     # printing the contact list
 
-elif command == 'show':
-    contact_to_show = input('Enter name and surname: ')
-    print(my_contacts[contact_to_show])
-    # showing phone number by name
+    elif command == 'show':
+        contact_to_show = input('Enter name and surname: ')
+        if contact_to_show in my_contacts:
+             print(my_contacts[contact_to_show])
+        else:
+            print("This contact does not exist.")
+    # showing phone number by name and surname
+    else:
+        print('Wrong command')
 
-else:
-    print('Wrong command')
 

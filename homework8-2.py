5cb2cee02f6e5c2e74765fa0428cc782926ee5db
ValentynaVel <110202655+ValentynaVel@users.noreply.github.com>
2023-01-13 13:40:33 +0000

    Adding homework#8

---
+++ homework8-2.py
@@ -0,0 +1,8 @@
+# 2. Написати кастомний Exception клас, MyCustomException, який має повідомляти "Custom exception is occured".
+
+class MyCustomExeption(Exception):
+    pass
+
+raise MyCustomExeption('Custom exception is occured')
+
+

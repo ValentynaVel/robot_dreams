ff6b720075d798e3390241a593700188b03911bb
ValentynaVel <110202655+ValentynaVel@users.noreply.github.com>
2023-01-24 23:10:40 +0000

    deleting files

--- homework8_2.py
+++
@@ -1,8 +0,0 @@
-# 2 Написати кастомний Exception клас, MyCustomException, який має повідомляти "Custom exception is occured".
-
-class MyCustomExeption(Exception):
-    pass
-
-raise MyCustomExeption('Custom exception is occured')
-
-

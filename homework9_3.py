c1533ed4bf9bd15f01314e8798c612a61f3ec608
ValentynaVel <110202655+ValentynaVel@users.noreply.github.com>
2023-02-17 10:43:35 +0000

    updating task3

--- homework9_3.py
+++ homework9_3.py
@@ -1,24 +1,25 @@
-# 3. В попередньо написаний кастомний Exception додати запис помилки і час її виникнення у файл.
+# 3. В попередньо написаний кастомний Exception додати запис помилки і час її
+# виникнення у файл.
 
-import traceback
-import datetime
+from datetime import datetime
 
 
 class MyCustomExeption(Exception):
-    "Raised when the input password is not correct."
-    pass
+    def __init__(self, message=f'Exception: Incorrect password.'):
+        self.message = message
+        with open('mistakes.txt', 'a') as print_file:
+            print(f'{self.message}, time: {datetime.now().isoformat()}', file=print_file)
+
 
 password = 1234
 
 try:
     input_password = int(input("Enter a password: "))
     if input_password != password:
-        raise MyCustomExeption
+        msg = MyCustomExeption()
     else:
         print("Eligible to enter the meeting.")
 
-except MyCustomExeption:
-    with open('mistakes.txt', 'a') as print_file:
-        print(f'Exception: Incorrect password. Time of exception: ', file=print_file)
-
+except MyCustomExeption():
+    raise MyCustomExeption
 

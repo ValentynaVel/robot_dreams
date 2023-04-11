fb8bf270f2b5668d317a3a50c2cf87435220ce81
ValentynaVel <110202655+ValentynaVel@users.noreply.github.com>
2023-04-11 20:42:21 +0100

    solving mistakes in code

--- 27/js.js
+++ 27/js.js
@@ -1,16 +1,13 @@
 const myDiv = document.createElement('div');
-const myDiv2 = document.createElement('div');
 myDiv.className = 'buttons';
 myDiv.style.background = 'white';
 myDiv.style.textAlign = 'center'
-myDiv2.className = 'paragraph';
-myDiv2.style.background = 'white';
-myDiv2.style.textAlign = 'left'
 
+// Пошук рандомного числа друзів
+let friendCount = Math.floor(Math.random() * 100) + 1;
+document.getElementById("friends").innerText = `Кількість друзів: ${friendCount}`;
 
-var numPool = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ],
-rand = numPool[Math.floor(Math.random() * numPool.length)];
-
+// Створення 3 кнопок
 ['Додати в друзі', 'Написати повідомлення', 'Запропонувати роботу'].map(buttonName => {
     let button = document.createElement('button');
     button.className = 'btn btn-info';
@@ -19,23 +16,18 @@ rand = numPool[Math.floor(Math.random() * numPool.length)];
     button.style.margin = '5px'
     myDiv.appendChild(button);
 })
-
 document.getElementsByTagName('body')[0].appendChild(myDiv);
 
-['Кількість друзів '].map(paragraphName => {
-    let paragraph = document.createElement('p');
-    paragraph.className = 'alert alert-success';
-    paragraph.innerText = `${paragraphName} ${rand}`;
-    paragraph.style.margin = '5px'
-    myDiv2.appendChild(paragraph);
-})
+// Додавання ивенту після кліку на button 'Додати в друзі'.
+let buttonFriends =  document.getElementsByClassName('btn btn-info')[0];
+let eventHandler = (event) => {
+    event.target.disabled = true;
+    buttonFriends.innerText = "Очікується підтвердження";
+    friendCount++;
+    document.getElementById("friends").innerText = `Кількість друзів: ${friendCount}`;
+}
+buttonFriends.addEventListener('click', eventHandler);
 
-document.getElementsByTagName('body')[0].appendChild(myDiv2);
 
-const btn = document.getElementById('1')[0];
-btn.addEventListener('click', (myVarEv) => {
-  paragraph.innerText = `${paragraphName} ${rand+1}`;
-  myVarEv.target.style.background = 'black';
-  myVarEv.target.disabled = true;
-});
+
 

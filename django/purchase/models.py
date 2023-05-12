8ef0bbaee43a5a6c644b7f7d32fed97cf393a1a9
ValentynaVel <110202655+ValentynaVel@users.noreply.github.com>
2023-05-12 09:15:08 +0100

    updating purchare/models.py

--- django/purchase/models.py
+++ django/purchase/models.py
@@ -5,8 +5,8 @@ from user.models import User
 
 class Purchase(models.Model):
     date = models.DateField()
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'purchase'

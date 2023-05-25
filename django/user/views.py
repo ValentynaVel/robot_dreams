97b33f2640a2fc419d60ca7533493ffb1796389f
ValentynaVel <110202655+ValentynaVel@users.noreply.github.com>
2023-05-25 13:27:46 +0100

    adding filter

--- django/user/views.py
+++ django/user/views.py
@@ -9,8 +9,18 @@ from rest_framework.paginator import PageNumberPaginator
 from rest_framework import filters
 
 
+class UserFilter(django_filters.FilterSet):
+    class Meta:
+        model = User
+        fields = {
+            'first_name': ['contains'],
+            'age': ['gte', 'lte', 'gt', 'lt', 'exact']
+        }
+
+
 class CustomPaginator(PageNumberPaginator):
     page_size_query_param = 'page_size'
+
 
 class UserListView(ListView):
     model = User
@@ -30,12 +40,18 @@ class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    filterset_class = UserFilter
+
+    search_fields = ['first_name', 'last_name', ]
+    ordering_fields = ['age', 'id']
+    pagination_class = CustomPaginator
+
+
+    max_page_size = 10
     filter_backends = [
+        django_filters.rest_framework.DjangoFilterBackend,
         filters.SeachFilter,
         filters.OderingFilter,
     ]
-    search_fields = ['first_name', 'last_name',]
-    ordering_fields = ['id']
 
-    pagination_class = CustomPaginator
-    max_page_size = 10
+

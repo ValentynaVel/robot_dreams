60e995bb061a0da15b7ddfcef0b1e4a515368eac
ValentynaVel <110202655+ValentynaVel@users.noreply.github.com>
2023-05-29 00:56:53 +0100

    adding task#47

--- django/robot/settings.py
+++ django/robot/settings.py
@@ -96,8 +96,12 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "robot",
+        "USER": "robot",
+        "PASSWORD": 'robot',
+        "HOST": "0.0.0.0",
+        "PORT": "5432",
     }
 }
 

f2e1a57b8a7e48d10ae65c7af69bd04370f51182
ValentynaVel <110202655+ValentynaVel@users.noreply.github.com>
2023-05-30 23:01:44 +0100

    tests for homework4

--- homework4.py
+++ homework4.py
@@ -14,6 +14,39 @@ if user_input.isdigit():
 else:
     print("This is word. It contains " + str(len(user_input)) + " letters.")
 
+import unittest
+from unittest.mock import patch
+from io import StringIO
+import sys
+from analyze_input import analyze_input
+
+
+class AnalyzeInputTest(unittest.TestCase):
+
+    @patch('builtins.input', return_value='8')
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_analyze_input_number_even(self, mock_stdout, mock_input):
+        analyze_input()
+        self.assertEqual(mock_stdout.getvalue().strip(), 'This is number 8. It is even.')
+
+    @patch('builtins.input', return_value='11')
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_analyze_input_number_odd(self, mock_stdout, mock_input):
+        analyze_input()
+        self.assertEqual(mock_stdout.getvalue().strip(), 'This is number 11. It is odd.')
+
+    @patch('builtins.input', return_value='Hello')
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_analyze_input_word(self, mock_stdout, mock_input):
+        analyze_input()
+        self.assertEqual(mock_stdout.getvalue().strip(), 'This is word. It contains 5 letters.')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
 
 
 
